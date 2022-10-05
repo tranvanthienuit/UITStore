270bba46f4edce7f747e26c347ed1de5959e3e13
tranvanthienuit <19522268@gm.uit.edu.vn>
2022-10-05 08:53:43 +0700

    Initial commit

---
+++ UITStore/UITStore/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UITStore
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage LoginPage = new NavigationPage(new Login());
+            NavigationPage Register = new NavigationPage(new Register());
+            
+            Children.Add(new Home());
+            Children.Add(LoginPage);
+            Children.Add(Register);
+        }
+    }
+}
---
+++ UITStore/UITStore/SneakersPage.xaml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SneakersUIApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UITStore
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SneakersPage : ContentPage
+    {
+        public SneakersPage()
+        {
+            InitializeComponent();
+            BindingContext = new SneakersViewModel();
+        }
+    }
+}
