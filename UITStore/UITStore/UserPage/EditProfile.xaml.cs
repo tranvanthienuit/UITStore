1dcd896e2cae7f0339707d623143bceceb1cffe5
tranvanthienuit <19522268@gm.uit.edu.vn>
2022-10-23 21:02:15 +0700

    up

--- UITStore/UITStore/UserPage/EditProfile.xaml.cs
+++ UITStore/UITStore/UserPage/EditProfile.xaml.cs
@@ -14,14 +14,20 @@ namespace UITStore.UserPage
         public EditProfile()
         {
             InitializeComponent();
+            User = new User();
+            userViews = new UserViews();
             User = Application.Current.Properties["user"] as User;
             //truyền userViews extend từ INotifyPropertyChanged ra ngoài khi các tham số của userViews ở views thay đổi
             //thì sẽ được cập nhật các tham số đó ở trong class userViews qua đó cập nhật các giá trị
-            userViews.username = User.username;
-            userViews.password = User.password;
-            userViews.fullname = User.fullname;
-            userViews.telephone = User.telephone;
-            userViews.address = User.address;
+            if (User != null)
+            {
+                userViews.username = User.username;
+                userViews.password = User.password;
+                userViews.fullname = User.fullname;
+                userViews.telephone = User.telephone;
+                userViews.address = User.address;
+            }
+
             BindingContext = userViews;
         }
 
