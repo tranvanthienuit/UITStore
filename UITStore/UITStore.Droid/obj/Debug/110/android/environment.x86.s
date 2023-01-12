54006e0008be8eb40a0ce758456e352c41df7e88
ChienPham231201 <19521286@gm.uit.edu.vn>
2023-01-12 21:45:38 +0700

    update the end

--- UITStore/UITStore.Droid/obj/Debug/110/android/environment.x86.s
+++ UITStore/UITStore.Droid/obj/Debug/110/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"072a9fcb-7e8f-40eb-940a-7558bcf55fa5"
+	.asciz	"65203c15-5289-47fe-b0a6-2d22c6ea60ee"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
